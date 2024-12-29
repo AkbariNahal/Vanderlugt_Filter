78b9a0c802fad7484ebc12b613194ca4f20a43cf
Nahal Akbari <AKBARIN@myumanitoba.ca>
2024-12-29 10:28:16 -0600

    Update Vanderlugt_Filter_MATLAB.m

--- Vanderlugt_Filter_MATLAB.m
+++ Vanderlugt_Filter_MATLAB.m
@@ -1,165 +1,163 @@
+% Vanderlugt Filter Implementation for Face Recognition
+% Authors: Nahal Akbari, Hassan Kaatuzian
+% Description: MATLAB implementation of Vanderlugt filter for facial recognition
+% using optical correlation techniques.
+
 clear all;
 close all; clc;
 warning off;
-%%
 
+%% Initialize Parameters and Load Data
+% Load the ORL face database
 db = 'olivettifaces.mat';
 load(db);
 N = size(faces,1);
-m=64,n=m,
-% Particular_Faces = 2;
-n_rows = round(sqrt(400));
+m=64; n=m;  % Image dimensions (64x64 pixels)
+n_rows = round(sqrt(400));  % For display purposes
 n_cols = n_rows;
-landa = 633.1e-6 ;
-theta = pi/4;
-alpha =sin(theta)/landa
-%
+
+% Set optical system parameters
+lambda = 633.1e-6;  % HeNe laser wavelength (mm)
+theta = pi/4;       % Reference beam angle
+alpha = sin(theta)/lambda;  % Spatial frequency
+
+%% Prepare Image Arrays
+% Initialize cell arrays for storing images
+Image = cell(1,400);    % All images
+Image1 = cell(1,200);   % Training set images
+    
+% Reshape all images to 64x64
 for i=1:400
-    Filename{i}=string("person" + i +".gif");
-    img=reshape(faces(:,i),m,n);
-    Image{i}=img;
+    img = reshape(faces(:,i),m,n);
+    Image{i} = img;
 end
 
+% Create training set (every other set of 5 images)
 for i=1:400
-    ii=fix(i/5);
-    if rem(ii,2)==0
-        iii=2.5*ii+mod(i,5);
+    ii = fix(i/5);
+    if rem(ii,2)==0  % Select even sets
+        iii = 2.5*ii + mod(i,5);
         im = Image{i};
-        Image1{iii}=im;
+        Image1{iii} = im;
     end
 end
-
 clear i ii iii img
 
-%%
+%% Generate Vanderlugt Filter
+% Create coordinate grid for reference wave
+x = linspace(-5,5,n);
+y = linspace(-5,5,m);
+[x,y] = meshgrid(x,y);
+U = 1*exp(2*pi*j.*alpha.*y);  % Reference wave
 
-% figure;clf;set(gcf,'Name','Faces');
-%
-% for ii=1:400
-%     ii;
-%     subplot(n_rows,n_cols,ii);
-%     %     imagesc(reshape(faces(:,ii),m,n));
-%     colormap gray; axis equal tight off;
-%     %     title(num2str(ii));
-% end
-
-%%
-% Vanderlugt filter dataset
-
-x=linspace(-5,5,n);
-y=linspace(-5,5,m);
-[x,y]=meshgrid(x,y);
-U =1*exp(2*pi*j.*alpha.*y);
-
+% Generate filters for each training image
 for i=1:200
-   
-        im0=Image1{i};
-        Fim0=fftshift(fft2(im0));
-        FF{i}=Fim0;
+    if ~isempty(Image1{i})
+        im0 = Image1{i};
+        Fim0 = fftshift(fft2(im0));  % Fourier transform
+        FF{i} = Fim0;  % Store Fourier transform
         
-        A=abs(Fim0);
-        Amax=max(max(A));
-        A=A./Amax;
-        phi=angle(Fim0);
-        Fi=A.*exp(j.*phi);
-        VF=abs(Fi+U).^2;
-        VFa{i}=VF;
-        % VF=im2uint8(VF);
+        % Normalize amplitude
+        A = abs(Fim0);
+        Amax = max(max(A));
+        A = A./Amax;
         
+        % Calculate phase
+        phi = angle(Fim0);
+        Fi = A.*exp(j.*phi);
+        
+        % Create Vanderlugt filter
+        VF = abs(Fi+U).^2;
+        VFa{i} = VF;
+    end
 end
-%
-%
 clear i ii iii
 
-%%
-% correlation between random pics and dataset
-w =217  %randi([1,26],1,1);
+%% Perform Correlation Analysis
+% Select test image
+w = 217;  % Test image index
+q = Image{w};
 
-q=Image{w};
-%
-cor=zeros(1,200);
-for k=1:200;
-    img=Image1{k}; %VFa{i};
+% Calculate correlation with all training images
+cor = zeros(1,200);
+for k=1:200
+    img = Image1{k};
     s = corrcoef(img,q);
-    %   s = corrcoef(img(:),q(:));
-    cor(k)=abs(s(1,2));
+    cor(k) = abs(s(1,2));
 end
-xt=find(cor==max(cor));
+xt = find(cor==max(cor));  % Find best match
 
+% Plot correlation results
 figure,stem(cor);title("correlation diagram");
 xlabel('pic num')
-ylabel('correlation coficient')
- 
-%
-Fim0=fftshift(fft2(q));
-FF0=Fim0;
-figure;imagesc(log(1+abs(FF0)));colormap(gray)
+ylabel('correlation coefficient')
+
+%% Process Test Image
+% Compute Fourier transform of test image
+Fim0 = fftshift(fft2(q));
+FF0 = Fim0;
+figure;imagesc(log(1+abs(FF0)));colormap(gray)  % Display Fourier spectrum
 set(gca,'XTick',[],'YTick',[]);
-%
-A=abs(Fim0);
-Amax=max(max(A));
-A=A./Amax;
-phi=angle(Fim0);
-Fi=A.*exp(j.*phi);
-VF=abs(Fi+U).^2;
-VFa0=VF;
-% TL=imadjust(TL,[],[],1);
+
+% Generate Vanderlugt filter for test image
+A = abs(Fim0);
+Amax = max(max(A));
+A = A./Amax;
+phi = angle(Fim0);
+Fi = A.*exp(j.*phi);
+VF = abs(Fi+U).^2;
+VFa0 = VF;
+
+% Display filter
 figure,imagesc(VFa0),colormap(gray);
 set(gca,'XTick',[],'YTick',[]);
-%
-%
-H=VFa0.*FF0;
-U=real((fft2(H)));
 
+%% Compute Final Correlation
+% Perform correlation operations
+H = VFa0.*FF0;
+U = real((fft2(H)));
+FF1 = FF{xt};
+H1 = VFa0.*FF1;
+U1 = real((fft2(H1)));
 
-FF1=FF{xt};
-H1=VFa0.*FF1;
-U1=real((fft2(H1)));
-
+% Display results
 figure;
-% clf;set(gcf,'Name','Faces');
-% subplot(3,1,1);
-imagesc(q),colormap(gray);
+imagesc(q),colormap(gray);  % Original image
 axis equal tight off;
 set(gca,'XTick',[],'YTick',[]);
-% subplot(3,1,2);
-figure;imagesc(abs(U));colormap(gray)
-set(gca,'XTick',[],'YTick',[]);
-axis equal tight off;
-%  title(num2str(ii));
-% subplot(3,1,3);
-figure;imagesc(abs(U1));colormap(gray)
+
+figure;imagesc(abs(U));colormap(gray)  % Correlation output 1
 set(gca,'XTick',[],'YTick',[]);
 axis equal tight off;
 
-%%
-cor0=zeros(1,200);
+figure;imagesc(abs(U1));colormap(gray)  % Correlation output 2
+set(gca,'XTick',[],'YTick',[]);
+axis equal tight off;
 
-cor=zeros(1,200);
-
+%% Calculate Error Rates
+% Compute correlations for all test images
+cor0 = zeros(1,200);
+cor = zeros(1,200);
 for i=1:400
-    i;
-    ii=fix(i/5);
-    if rem(ii,2)==1
-        iii=2.5*(ii-1)+mod(i,5)+1;
-        q=Image{i};
-        for k=1:200;
-            img=Image1{k}; 
+    ii = fix(i/5);
+    if rem(ii,2)==1  % Select odd sets for testing
+        iii = 2.5*(ii-1)+mod(i,5)+1;
+        q = Image{i};
+        for k=1:200
+            img = Image1{k}; 
             s = corrcoef(img,q);
-            cor0(k)=abs(s(1,2));
+            cor0(k) = abs(s(1,2));
         end
-%         xt=find(cor==max(cor0));
-        cor(iii)=max(cor0);
+        cor(iii) = max(cor0);
     end
 end
 
+% Plot final results
 figure,stem(cor);title("correlation");
 xlabel('pic num')
-ylabel('correlation coficient')
-
-erorcor=(1-cor)*100;
+ylabel('correlation coefficient')
+erorcor = (1-cor)*100;  % Calculate error percentage
 figure,stem(erorcor);title("error");
 xlabel('pic num')
 ylabel('error percent')
-
-M = mean(erorcor)
+M = mean(erorcor)  % Calculate mean error
